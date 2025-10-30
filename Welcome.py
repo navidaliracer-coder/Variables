888c69ecdd84c5a56fdc022793e3b0cc372dcbb2
navidaliracer-coder <navidali.racer@gmail.com>
2025-10-30 17:32:35 -0400

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,19 @@
+#Write a program for print command.
+
+#Print a sentence
+print("Welcome to the world of python")
+
+#print a number
+print(9)
+
+#next line after a print
+print("hello python\n")
+
+#pass mutiple arguments for printing
+print("hello",8)
+
+#end argument of print statement 
+print("welcome to",end = "*")
+
+
+
