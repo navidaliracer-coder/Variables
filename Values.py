b5ae3c01a76c83d82c146149ba200cdb26328ae5
navidaliracer-coder <navidali.racer@gmail.com>
2025-10-30 17:43:17 -0400

    Add files via upload

---
+++ Values.py
@@ -0,0 +1,14 @@
+#Write a program to create different variables to store various values.
+x = 5 
+y = "Navid"
+print(x)
+print(y)
+
+codingal = "codingal"
+print (codingal)
+
+#user input
+name = input("enter your name")
+
+print ("\nHello", name,"\nwelcome to codingal")
+
