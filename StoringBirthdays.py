5733016920b9ea4b0cc4981ddb893bed483ee464
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-02 11:09:44 -0500

    Add files via upload

---
+++ StoringBirthdays.py
@@ -0,0 +1,25 @@
+#Write a program to store birthday dates inside 5 diffrent varibles
+
+#Birthday varibles 
+Navids_birthday = "January 13"
+
+Bobs_birthday = "June 23"
+
+Kevins_birthday = "Febuary 15"
+
+Steves_birthday = "December 15"
+
+Billys_birthday = "october 19"
+
+#Printing birthday varibles 
+print(Navids_birthday)
+
+print(Bobs_birthday)
+
+print(Kevins_birthday)
+
+print(Steves_birthday)
+
+print(Billys_birthday)
+
+
