14d47f4aa6d3f5f69eddc5a678cf064a5d539f8f
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-04 18:55:16 -0500

    Add files via upload

---
+++ String.py
@@ -0,0 +1,7 @@
+# This project aims to get a clear understanding of how to perform operations on a string.
+text = "Congratulations Ram"
+print(text)
+
+upper_text = text.upper()
+print(upper_text)
+# Output: CONGRATULATIONS RAM
