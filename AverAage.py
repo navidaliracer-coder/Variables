4aa57f993a2883e1d67aea183045cc8824a943bd
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-06 16:53:12 -0500

    Add files via upload

---
+++ AverAage.py
@@ -0,0 +1,14 @@
+#storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95 
+tree5 = 11
+
+#finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 trees is: ", sum)
+
+#finding the average of the trees height 
+average = sum/5
+print("the average of all the trees height is :", average)
