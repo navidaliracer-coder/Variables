ce2a0d9db7964f7d53bf1956b5df8102496b8173
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-10 17:42:48 -0500

    Add files via upload

---
+++ greater or less.py
@@ -0,0 +1,7 @@
+YourNumber=int(input("enter a number: "))
+if (YourNumber < 15): 
+    print ("Your Number is smaller then 15")
+    print("Your Number is an if statement")
+else:
+    print("Your Number is greater then 15")
+    print("Your Number is an if statement")
