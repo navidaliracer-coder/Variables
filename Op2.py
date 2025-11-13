dee5552eabdc3a04d152af9f7cb89a60018bab4f
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-13 17:06:32 -0500

    Add files via upload

---
+++ Op2.py
@@ -0,0 +1,12 @@
+#Write a program to check if the given values have boolean or not
+
+a = 23
+b = 24
+c = 22
+
+if a > b or a < c:
+    print("I am using an or operater")
+elif a < b or a > c:
+    print("I am using a and operater")
+
+    
