15dd1412b47fdc67b09f99b9d7d7e683d224806c
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-24 17:29:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+a = 12
+b = 3
+c = 4 
+d = 9
+answer = b**c + d/ b*c + c
+print (answer)
+
+
+n1=int(input("Enter numerator: "))
+n2=int(input("Enter denomenator: "))
+if n1%n2== 0: 
+    print(str(n1)+" is divisble by "+ str (n1))
+else:    
+     print(str(n1)+" is divisble by "+ str (n2))
+     
