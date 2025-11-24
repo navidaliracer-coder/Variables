24dc07cca41ef1839b6a055cdf754c6e13405728
navidaliracer-coder <navidali.racer@gmail.com>
2025-11-24 17:40:09 -0500

    Add files via upload

---
+++ meanValue.py
@@ -0,0 +1,16 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+#sum of 40 numbers
+sum = mean1*total_number
+print("The sum of 40 numbers is: ",sum)
+
+
+#Correct sum of numbers
+num2 = sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ", num2)
+
+#the correct mean
+mean2=num2/total_number
+print(mean2)
