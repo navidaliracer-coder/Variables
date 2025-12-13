f89c4590e7524b698f96f3b48ccb9f43ce841a6a
navidaliracer-coder <navidali.racer@gmail.com>
2025-12-13 13:17:24 -0500

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,13 @@
+age = int(input("Enter you age:"))
+
+if 10 <= num <= 20:
+    print("Your age is between 10-20")
+else: print("Your are not between 10-20")
+
+if 0 <= num <= 10:
+    print("Your age is between 0-10")
+else: print("Your are not between 10-20")
+
+
+
+
