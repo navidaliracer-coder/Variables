edc28473045c129a2d2d9011628e58b1a992297b
navidaliracer-coder <navidali.racer@gmail.com>
2025-12-24 10:46:51 -0500

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,23 @@
+def employee(name):
+    print(name)
+
+def salary(exp):
+    if exp >= 5:
+        return 3000000
+    elif exp >= 3:
+        return 1000000
+    else:
+        return 500000
+
+# Function calls
+employee("Bob")
+a = salary(15)
+print("The salary of the employee is", a)
+
+
+
+
+
+
+
+    
