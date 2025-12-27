3c290b06b89539c77495a6f377e070352fadbcff
navidaliracer-coder <navidali.racer@gmail.com>
2025-12-27 14:14:48 -0500

    Add files via upload

---
+++ A break.py
@@ -0,0 +1,21 @@
+#Write a program to check alphabet “A” is present in the given string or not. And terminate the loop after finding the alphabet “A.”
+
+#Take user input 
+a = input("Enter your word: ")
+
+#Program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1 
+#display result
+      print ("A is found")
+      break
+
+else:
+    print("A is not in your word")
+    
+
+               
+
+
+
+
