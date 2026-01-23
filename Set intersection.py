691ee05579123e812b907023c247cb68cb3d3800
navidaliracer-coder <navidali.racer@gmail.com>
2026-01-23 17:54:07 -0500

    Add files via upload

---
+++ Set intersection.py
@@ -0,0 +1,10 @@
+#Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}
+
+setx = {"Green", "Blue"}
+sety = {"Blue", "Yellow"}
+print("The og sets")
+print(setx)
+print(sety)
+print("\nIntersection of 2 sets")
+setz = setx.intersection(sety)
+print(setz)
