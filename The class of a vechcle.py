fab6628134a397b70dacfdeab4327b268cdfe2a7
navidaliracer-coder <navidali.racer@gmail.com>
2026-02-02 17:27:59 -0500

    Add files via upload

---
+++ The class of a vechcle.py
@@ -0,0 +1,13 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+Camaro = vehicle(240, 18)
+
+print("The Camaro's max speed is", Camaro.max_speed)
+print("The Camaro's mileage is", Camaro.mileage)
+
+
+
+        
