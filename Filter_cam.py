673dd3c9518dc544efac5e5b530074ab6670ab06
navidaliracer-coder <navidali.racer@gmail.com>
2026-06-09 20:29:17 -0400

    Add files via upload

---
+++ Filter_cam.py
@@ -0,0 +1,77 @@
+#Students will experiment with applying real-time color filters and edge detection effects to a sample image or webcam feed. They will toggle between filters and edge detection techniques using keyboard input and observe the effects instantly.
+
+import cv2
+import numpy
+
+def apply_filter(image, ftype):
+    """Apply a filter to the image based on the filter type"""
+    img = image.copy()
+    if ftype == "red_tint":
+        img[:, :, 1] = img[:, :, 0] = 0
+    
+    elif ftype == "green_tint":
+        img[:, :, 0] = img[:, :, 2] = 0
+    
+    elif ftype == "blue_tint":
+        img[:, :, 1] = img[:, :, 2] = 0
+    
+    elif ftype == "sobel":
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        sx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+        sy = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+        sob = cv2.bitwise_or(sx.astype('uint8'), sy.astype('uint8'))
+        img = cv2.cvtColor(sob, cv2.COLOR_BGR2RGB)
+    
+    elif ftype == "Canny":
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        gray = cv2.medianBlur(gray, 5)
+        edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+        
+        color = cv2.bilateralFilter(image, 9, 300, 300)
+        img = cv2.bitwise_and(color, color, mask = edges)
+    return img
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        return
+    ftype = "original"
+    print("Keys: R=red,  G=green, b=blue, s=sobel, c=canny, t=cartoon, q=quit")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't recive the frame")
+            break
+        out = apply_filter(frame, ftype)
+        cv2.imshow("filter", out)
+        key = cv2.waitKey(1) & 0xff
+        if key == ord('r'):
+            ftype = "red_tint"
+        elif key == ord('g'):
+            ftype = "green_tint"
+        elif key == ord('b'):
+            ftype = "blue_tint"
+        elif key == ord('s'):
+            ftype = "sobel"
+        elif key == ord('c'):
+            ftype = "canny"
+        elif key == ord('t'):
+            ftype = "cartoon"
+        elif key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+    
